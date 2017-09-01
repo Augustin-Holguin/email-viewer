1994c63d0d234b1f8532278227c101dab231215e
gusholguin <augustin.holguin@gmail.com>
2017-09-01 15:54:32 +0200

    email view nearly done

--- app/assets/javascripts/email.js
+++ app/assets/javascripts/email.js
@@ -3,5 +3,37 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-  $('#output').hide();
+  $('#mail1').click(function(){
+    $('.mail-view, #output1').show();
+    $('#output2').hide();
+    $('#output3').hide();
+    $('#output4').hide();
+  })
+});
+
+$(document).ready(function() {
+  $('#mail2').click(function(){
+    $('#mail-view, #output2').show();
+    $('#output1').hide();
+    $('#output3').hide();
+    $('#output4').hide();
+  })
+});
+
+$(document).ready(function() {
+  $('#mail3').click(function(){
+    $('#mail-view, #output3').show();
+    $('#output1').hide();
+    $('#output2').hide();
+    $('#output4').hide();
+  })
+});
+
+$(document).ready(function() {
+  $('#mail4').click(function(){
+    $('#mail-view, #output4').show();
+    $('#output1').hide();
+    $('#output3').hide();
+    $('#output2').hide();
+  })
 });
